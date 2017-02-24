72c18e2af5e09e9b9a181ac7075bdc99ab2105ec
oliver <oliver@Kali.local.lan>
2017-02-24 14:31:02 +0000

    Modificación enlace en el menú convertir de la plantilla. Añadida la clase controlador de archivos

---
+++ app/controladores/archivos.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\controladores\archivos;
+
+use app\Api\Api;
+use app\interfaz\Rest\Rest;
+
+//use modelo archivos
+
+/**
+ * Description of archivos
+ *
+ * @author oliver
+ */
+class archivos extends Api implements Rest {
+
+    /*POST*/
+    public function alta() {
+        
+    }
+    
+    public function baja() {
+        
+    }
+    
+    public function modificar() {
+        
+    }    
+    /*GET*/
+    public function listar() {
+        
+    }
+    public function ver($id) {
+        
+    }
+    
+    public function convertir() {
+        echo "Class archivos -- Método convertir()";
+    }
+}

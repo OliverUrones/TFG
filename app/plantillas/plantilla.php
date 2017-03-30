6d59c7f641344a3456fbccb653e532efeef2684f
Oliver Urones <oliver@oliver-desktop>
2017-03-30 14:03:26 +0200

    Añadidos archivos para el Drag And Drop: dropzone.css, dropzone.js, ng-dropzone.min.css, ng-dropzone.min.js, dragAndDropController.js Modificada plantilla para añadir los scripts y los css para el Drag And Drop. Modificada vista convertir.php para crear la zona Drag And Drop.

--- app/plantillas/plantilla.php
+++ app/plantillas/plantilla.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" href="web/css/estilos.css">
         <link rel="stylesheet" href="web/css/ngDialog-theme-default.min.css">
         <link rel="stylesheet" href="web/css/ngDialog.min.css">
-        <link href="web/css/dropzone.css" rel="stylesheet" type="text/css"/>
         <link href="web/css/ng-dropzone.min.css" rel="stylesheet" type="text/css"/>
+        <link href="web/css/dropzone.css" rel="stylesheet" type="text/css"/>
         <script src="web/js/angular.min.js"></script>
         <script src="web/js/ngDialog.min.js"></script>
         <script src="web/js/jquery.js"></script>
